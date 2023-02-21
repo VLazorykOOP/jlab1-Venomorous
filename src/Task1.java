67bde7fe791a42ff15cac2fb16f58fb0d164610a
Venomorous <94299601+Venomorous@users.noreply.github.com>
2023-02-21 11:07:03 +0200

    Added more functional to tasks 1 and 2

--- src/Task1.java
+++ src/Task1.java
@@ -12,6 +12,7 @@ public class Task1 {
 //        System.out.println("\n");
         int aint = 0, bint;
         double ad, bd;
+        int badInput = 0;
 
         int resint;
         double resd;
@@ -23,145 +24,196 @@ public class Task1 {
             System.out.println("3. Exit");
 
             System.out.print("Choose an option: ");
-            int opt = scan.nextInt();
-            System.out.print("\n");
-            int opt2 = 0;
+            if (scan.hasNextInt()) {
+                int opt = scan.nextInt();
+                System.out.print("\n");
+                int opt2 = 0;
+                badInput = 0;
 
-            switch (opt) {
-                case 1:
-                    while (opt2 != 3) {
-                        System.out.println("------------------------------");
-                        System.out.println("What type do you want result to be?");
-                        System.out.println("1. Integers");
-                        System.out.println("2. Double");
-                        System.out.println("3. Back");
+                switch (opt) {
+                    case 1:
+                        while (opt2 != 3) {
+                            badInput = 0;
+                            System.out.println("------------------------------");
+                            System.out.println("What type do you want result to be?");
+                            System.out.println("1. Integers");
+                            System.out.println("2. Double");
+                            System.out.println("3. Back");
 
-                        System.out.print("Choose an option: ");
-                        opt2 = scan.nextInt();
-                        System.out.print("\n");
+                            System.out.print("Choose an option: ");
+                            if (scan.hasNextInt()) {
+                            opt2 = scan.nextInt();
+                            System.out.print("\n");
 
-                        switch (opt2) {
-                            case 1:
-                                System.out.print("Enter a: ");
-                                if(scan.hasNextInt()) {
-                                    aint = scan.nextInt();
+                                switch (opt2) {
+                                    case 1:
+                                        System.out.print("Enter a: ");
+                                        if (scan.hasNextInt()) {
+                                            aint = scan.nextInt();
 
-                                    System.out.print("\nEnter b: ");
-                                    if(scan.hasNextInt()) {
-                                    bint = scan.nextInt();
+                                            System.out.print("\nEnter b: ");
+                                            if (scan.hasNextInt()) {
+                                                bint = scan.nextInt();
 
-                                    resint = (aint * bint - (aint + bint) * (aint - bint)) / (bint ^ 4 + aint ^ 3) + 5 * bint;
-                                    System.out.print("\nResult is: " + resint + "\n");
-                                    } else {
-                                        scan.next();
-                                        System.out.println("\nWrong input!!!");
-                                    }
+                                                resint = (aint * bint - (aint + bint) * (aint - bint)) / (bint ^ 4 + aint ^ 3) + 5 * bint;
+                                                System.out.print("\nResult is: " + resint + "\n");
+                                            } else {
+                                                badInput++;
+                                                if (badInput == 3) {
+                                                    System.out.println("You entered wrong input 3 times");
+                                                    System.exit(0);
+                                                }
+                                                scan.next();
+                                                System.out.println("\nWrong input!!!");
+                                            }
 
-                                }else {
-                                    scan.next();
-                                    System.out.println("\nWrong input!!!");
-                                }
-                                break;
-                            case 2:
-                                System.out.print("Enter a: ");
-                                if(scan.hasNextInt()) {
-                                    aint = scan.nextInt();
+                                        } else {
+                                            badInput++;
+                                            if (badInput == 3) {
+                                                System.out.println("You entered wrong input 3 times");
+                                                System.exit(0);
+                                            }
+                                            scan.next();
+                                            System.out.println("\nWrong input!!!");
+                                        }
+                                        break;
+                                    case 2:
+                                        System.out.print("Enter a: ");
+                                        if (scan.hasNextInt()) {
+                                            aint = scan.nextInt();
 
-                                    System.out.print("\nEnter b: ");
-                                    if(scan.hasNextInt()) {
-                                        bint = scan.nextInt();
+                                            System.out.print("\nEnter b: ");
+                                            if (scan.hasNextInt()) {
+                                                bint = scan.nextInt();
 
-                                        ad = aint;
-                                        bd = bint;
+                                                ad = aint;
+                                                bd = bint;
 
-                                        resd = (ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd;
-                                        System.out.print("\nResult is: " + resd + "\n");
-                                    } else {
-                                        scan.next();
-                                        System.out.println("\nWrong input!!!");
-                                    }
+                                                resd = (ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd;
+                                                System.out.print("\nResult is: " + resd + "\n");
+                                            } else {
+                                                badInput++;
+                                                if (badInput == 3) {
+                                                    System.out.println("You entered wrong input 3 times");
+                                                    System.exit(0);
+                                                }
+                                                scan.next();
+                                                System.out.println("\nWrong input!!!");
+                                            }
 
-                                }else {
-                                    scan.next();
-                                    System.out.println("\nWrong input!!!");
-                                }
-                                break;
+                                        } else {
+                                            badInput++;
+                                            if (badInput == 3) {
+                                                System.out.println("You entered wrong input 3 times");
+                                                System.exit(0);
+                                            }
+                                            scan.next();
+                                            System.out.println("\nWrong input!!!");
+                                        }
+                                        break;
 //                            case 3:
 //                                System.exit(0);
-                            default:
-                                System.out.println("There is no option for this number \n");
+                                    default:
+                                        System.out.println("There is no option for this number \n");
+                                }
+                            } else {
+                                scan.next();
+                                System.out.println("You can use numbers only!!!");
+                            }
                         }
-                    }
-                    break;
-                case 2:
-                    while (opt2 != 3) {
-                        System.out.println("------------------------------");
-                        System.out.println("What type do you want result to be?");
-                        System.out.println("1. Integers");
-                        System.out.println("2. Double");
-                        System.out.println("3. Back");
+                        break;
+                    case 2:
+                        while (opt2 != 3) {
+                            System.out.println("------------------------------");
+                            System.out.println("What type do you want result to be?");
+                            System.out.println("1. Integers");
+                            System.out.println("2. Double");
+                            System.out.println("3. Back");
 
-                        System.out.print("Choose an option: ");
-                        opt2 = scan.nextInt();
-                        System.out.print("\n");
+                            System.out.print("Choose an option: ");
+                            opt2 = scan.nextInt();
+                            System.out.print("\n");
 
-                        switch (opt2) {
-                            case 1:
-                                System.out.print("Enter a: ");
-                                if(scan.hasNextDouble()) {
-                                    ad = scan.nextDouble();
-
-                                    System.out.print("\nEnter b: ");
+                            switch (opt2) {
+                                case 1:
+                                    System.out.print("Enter a: ");
                                     if (scan.hasNextDouble()) {
-                                        bd = scan.nextDouble();
+                                        ad = scan.nextDouble();
 
-                                        resint = (int) ((ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd);
-                                        System.out.print("\nResult is: " + resint + "\n");
+                                        System.out.print("\nEnter b: ");
+                                        if (scan.hasNextDouble()) {
+                                            bd = scan.nextDouble();
+
+                                            resint = (int) ((ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd);
+                                            System.out.print("\nResult is: " + resint + "\n");
+                                        } else {
+                                            badInput++;
+                                            if (badInput == 3) {
+                                                System.out.println("You entered wrong input 3 times");
+                                                System.exit(0);
+                                            }
+                                            scan.next();
+                                            System.out.println("\nWrong input!!!");
+                                        }
+
                                     } else {
+                                        badInput++;
+                                        if (badInput == 3) {
+                                            System.out.println("You entered wrong input 3 times");
+                                            System.exit(0);
+                                        }
                                         scan.next();
                                         System.out.println("\nWrong input!!!");
                                     }
 
-                                }else {
-                                    scan.next();
-                                    System.out.println("\nWrong input!!!");
-                                }
-
-                                break;
-                            case 2:
-                                System.out.print("Enter a: ");
-                                if(scan.hasNextDouble()) {
-                                    ad = scan.nextDouble();
-
-                                    System.out.print("\nEnter b: ");
+                                    break;
+                                case 2:
+                                    System.out.print("Enter a: ");
                                     if (scan.hasNextDouble()) {
-                                        bd = scan.nextDouble();
+                                        ad = scan.nextDouble();
 
-                                        resd = (ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd;
-                                        System.out.print("\nResult is: " + resd + "\n");
+                                        System.out.print("\nEnter b: ");
+                                        if (scan.hasNextDouble()) {
+                                            bd = scan.nextDouble();
+
+                                            resd = (ad * bd - (ad + bd) * (ad - bd)) / (bd * bd * bd * bd + ad * ad * ad) + 5 * bd;
+                                            System.out.print("\nResult is: " + resd + "\n");
+                                        } else {
+                                            badInput++;
+                                            if (badInput == 3) {
+                                                System.out.println("You entered wrong input 3 times");
+                                                System.exit(0);
+                                            }
+                                            scan.next();
+                                            System.out.println("\nWrong input!!!");
+                                        }
+
                                     } else {
+                                        badInput++;
+                                        if (badInput == 3) {
+                                            System.out.println("You entered wrong input 3 times");
+                                            System.exit(0);
+                                        }
                                         scan.next();
                                         System.out.println("\nWrong input!!!");
                                     }
-
-                                }else {
-                                    scan.next();
-                                    System.out.println("\nWrong input!!!");
-                                }
-                                break;
+                                    break;
 //                            case 3:
 //                                System.exit(0);
-                            default:
-                                System.out.println("There is no option for this number \n");
+                                default:
+                                    System.out.println("There is no option for this number \n");
+                            }
                         }
-                    }
-                    break;
-                case 3:
-                    System.exit(0);
-                default:
-                    System.out.println("There is no option for this number \n");
+                        break;
+                    case 3:
+                        System.exit(0);
+                    default:
+                        System.out.println("There is no option for this number \n");
+                }
+            }else{
+                scan.next();
+                System.out.println("You can use numbers only!!!");
             }
         }
-
     }
 }
--- src/Task2.java
+++ src/Task2.java
@@ -6,42 +6,69 @@ public class Task2 {
         Scanner scan = new Scanner(System.in);
         int count = 0;
         double summa = 0, summa2 = 0;
+        int badInput=0;
 
+        while(badInput!=3){
         System.out.println("This program will create an array A[2n]");
         System.out.print("Enter n: ");
-        int n = scan.nextInt();
+        if(scan.hasNextInt()) {
+            int n = scan.nextInt();
+            if(n>0){
+            badInput = 0;
 
-        System.out.print("Enter min value of the range: ");
-        int min = scan.nextInt();
-        System.out.print("Enter max value of the range: ");
-        int max = scan.nextInt();
+            System.out.print("Enter min value of the range: ");
+            if (scan.hasNextInt()) {
+                int min = scan.nextInt();
+                System.out.print("Enter max value of the range: ");
+                if (scan.hasNextInt()) {
+                    int max = scan.nextInt();
 
-        double A[] = new double [2*n];
+                    double A[] = new double[2 * n];
 
-        for(int i = 0; i < A.length; i++){
-            A[i] = Math.floor(Math.random() * (max - min + 1) + min);
-            summa += A[i];
-            if(i < n) {
-                summa2 += A[i];
-                count++;
-            }
-        }
+                    for (int i = 0; i < A.length; i++) {
+                        A[i] = Math.floor(Math.random() * (max - min + 1) + min);
+                        summa += A[i];
+                        if (i < n) {
+                            summa2 += A[i];
+                            count++;
+                        }
+                    }
 
-        double aver = summa2/n;
-        double summa3 = 0;
+                    double aver = summa2 / n;
+                    double summa3 = 0;
 
-        for(int i = n; i < A.length; i++){
-            if(A[i]>aver)
-                summa3 += A[i];
-        }
+                    for (int i = n; i < A.length; i++) {
+                        if (A[i] > aver)
+                            summa3 += A[i];
+                    }
 
-        for(int i = 0; i < A.length; i++){
-            System.out.println(A[i]);
-        }
-        System.out.println("Summa(all): " + summa);
-        System.out.println("Summa(n - 2n-1): " + summa2);
-        System.out.println("Average(n - 2n-1): " + aver);
+                    for (int i = 0; i < A.length; i++) {
+                        System.out.print(A[i] + " ");
+                    }
+                    System.out.println("\nSumma(all): " + summa);
+                    System.out.println("Summa(n - 2n-1): " + summa2);
+                    System.out.println("Average(n - 2n-1): " + aver);
 //        System.out.println("Count(0 - n-1): " + count);
-        System.out.println("Summa(>average): " + summa3);
+                    System.out.println("Summa(>average): " + summa3);
+                    break;
+                } else {
+                    badInput++;
+                    System.out.println("Wrong input!!!");
+                }
+            } else {
+                badInput++;
+                System.out.println("Wrong input!!!");
+            }
+        }else{
+//                scan.next();
+                badInput++;
+                System.out.println("Only positive integer numbers!!!");
+            }
+    }else{
+            scan.next();
+            badInput++;
+            System.out.println("Only positive integer numbers!!!");
+        }
+    }
     }
 }
