9ec782f15209b685876a352582ec57253e021fc4
Venomorous <94299601+Venomorous@users.noreply.github.com>
2023-02-28 10:19:17 +0200

    Added latin checker to task 4

--- src/Task4.java
+++ src/Task4.java
@@ -14,14 +14,17 @@ public class Task4 {
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
             fLet = word.charAt(0);
-            System.out.println("Length:" + word.length());
+//            System.out.println("Length:" + word.length());
             for(int j = 1; j<word.length(); j++){
-                System.out.println("Index j: " + j);
-                if(word.charAt(j) == fLet){
+//                System.out.println("Index j: " + j);
+                if(Character.UnicodeBlock.of(word.charAt(j)) != Character.UnicodeBlock.BASIC_LATIN){
+                    System.out.println("\nOnly basic latin characters");
+                    break;
+                }else if(word.charAt(j) == fLet){
 //                    System.out.println("This word " + word + " has letter");
                       word = word.substring(0, j) + word.substring(j+1);
                       j--;
-                    System.out.println("Letter deleted");
+//                    System.out.println("Letter deleted");
                 }
             }
 //            word = word.substring(0, 0) + word.substring(1);
